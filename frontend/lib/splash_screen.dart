c021064bb539ff30a38a7b0236f0fdf6505a4c63
MevinduKalubowila <mevindu.20240337@iit.ac.lk>
2026-03-06 02:28:05 +0530

    splash_screen.dart
    Declare controllers & animations for rich splash effects

--- frontend/lib/splash_screen.dart
+++ frontend/lib/splash_screen.dart
@@ -1,1 +1,11 @@
 // Splash screen updating old code
+import 'dart:math' as math;
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+// Declare controllers & animations for rich splash effects
