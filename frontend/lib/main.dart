fc5231922d42c54d246f02b89551f01ef983a33e
MevinduKalubowila <mevindu.20240337@iit.ac.lk>
2026-03-05 14:42:41 +0530

    main.dart
    Added WidgetsFlutterBinding.ensureInitialized()

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,3 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'splash_screen.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Force portrait mode
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  // Make status bar transparent so gradient bleeds to top
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.light,
+    ),
+  );
+
+  runApp(const NutriFlexApp());
+}
