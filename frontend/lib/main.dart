963363947b5805fe31b7fa2f26574290bf9f300a
hasith20230011-netizen <hasith.20230011@iit.ac.lk>
2026-03-06 21:41:06 +0530

    Modified main.dart code due to login and signup pages not separately displaying

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'firebase_options.dart';
 import 'login_page.dart';
-import 'home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
@@ -15,17 +18,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final user = FirebaseAuth.instance.currentUser;
-
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: user != null && user.emailVerified
-          ? const HomePage()
-          : const LoginPage(),
+      home: LoginPage(), // ✅ Start from Login Page
     );
   }
 }
-
-
-
-
