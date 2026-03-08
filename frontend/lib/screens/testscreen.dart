d0f263a7c4c2a5c083174259476dd1a8a2d5b890
Kavindu Chandupa <kavinduchandupa856@gmail.com>
2026-03-08 15:22:13 +0530

    Modify the initial file structure for frontend, inside the lib folder

---
+++ frontend/lib/screens/testscreen.dart
@@ -0,0 +1,6 @@
+// rename this file name according to your purpose.
+
+// add all ui screen codes in this folder only.
+
+// eg:  bmi_screen.dart
+//      home_page.dart
---
+++ frontend/lib/services/api_service.dart
@@ -0,0 +1,5 @@
+// add your all api services dart codes in this particular folder.
+
+// rename this file if you want and add the code block here.
+
+// if you want to add more code lines create another files in the 'service' folder.
